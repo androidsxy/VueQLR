17514020cfa568eabaa7d556f9eb1c7d12ceeefb
androidsxy <1274187367@qq.com>
2019-01-24 20:55:55 +0800

    'change'

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,43 @@
+<template>
+<div class='wrapper'>
+    <swiper :options="swiperOption">
+    <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class='swiper-img' :src="item.imUrl"/>
+    </swiper-slide>
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+  </div>
+</template>
+<script>
+export default{
+    name:'HomeSwiper',
+    data(){
+        return{
+            swiperOption:{
+                pagination:".swiper-pagination",
+                loop:true
+            },
+            swiperList:[{
+                id:'001',
+                imUrl:'https://img1.qunarzz.com/order/comp/1805/2e/6e407f088bfb902.png'
+            },{
+                id:'002',
+                imUrl:'https://img1.qunarzz.com/order/comp/1805/2e/6e407f088bfb902.png'
+            }]
+        }
+    }
+}
+
+</script>
+<style lang='stylus' scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+    background :#fff !important
+.wrapper
+  touch-action: none;
+  overflow hidden
+  width:100%
+  height:0
+  padding-bottom:31.25%
+  .swiper-img
+    width:100%
+</style>
