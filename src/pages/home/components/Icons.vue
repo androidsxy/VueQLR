d8463118833fce288d41a04b47ed4ea71be98aad
androidsxy <1274187367@qq.com>
2019-03-22 12:52:47 +0800

    轮播图图

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="icons">
+    <div class="icon"></div>
+    <div class="icon"></div>
+    <div class="icon"></div>
+    <div class="icon"></div>
+    <div class="icon"></div>
+    <div class="icon"></div>
+    <div class="icon"></div>
+    <div class="icon"></div>
+</div>
+</template>
+
+<script>
+export default{
+    name:'HomeIcons'
+}
+
+</script>
+
+
+<style lang="stylus" scoped>
+.icons
+  width:100%
+  overflow:hidden
+  height:0
+  padding-bottom:50%
+  background:green
+  .icon
+    float:left
+    width:25%
+    padding-bottom:25%  
+    background:red
+</style>
